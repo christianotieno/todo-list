d53c2d31a960371f6310c69fc670f802c5ac0ced
Christian Otieno <oduorchristian@gmail.com>
2020-04-08 01:50:34 +0300

    Fix linter errors

--- src/dom/project-input.js
+++ src/dom/project-input.js
@@ -1,21 +1,20 @@
-const projectInput = document.querySelector('.project-input');
-
 const getProjectInput = () => {
+  const projectInput = document.querySelector('.project-input').value;
   let value = false;
 
   const emptyCheckDiv = document.querySelector('#project-empty-check');
-  if (projectInput.value === '') {
+  if (projectInput === '') {
     emptyCheckDiv.classList.remove('d-none');
     setTimeout(() => { emptyCheckDiv.classList.add('d-none'); }, 3000);
     value = false;
   } else {
-    value = projectInput.value;
+    value = projectInput;
   }
   return value;
 };
 
 const clearProjectField = () => {
-  projectInput.value = '';
+  document.querySelector('.project-input').value = '';
 };
 
 export { getProjectInput, clearProjectField };
